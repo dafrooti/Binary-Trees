8bbbc8b7188ed80c78116665693b2d8d1365c5d1
shruti <89041547+dafrooti@users.noreply.github.com>
2025-01-05 11:01:37 -0800

    Add files via upload

---
+++ Binary Trees/main.py
@@ -0,0 +1,94 @@
+class Tree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+    def display(self):
+        print(self.value, self.left, self.right)
+
+def insert(root, k):
+    if root == None:
+        return Tree(k)
+    if root.value > k:
+        root.left = insert(root.left, k)
+    else:
+        root.right = insert(root.right, k)
+    return root
+
+def search(root, key):
+    if root.value == key:
+        return root
+    elif root.value > key and root.left is not None:
+        return search(root.left, key)
+    elif root.value < key and root.right is not None:
+        return search(root.right, key)
+    else:
+        return -1
+
+def mostleft(root):
+    if root == None:
+        return None
+    while root.left:
+        root = root.left
+    return root.left
+
+def inorder_traversal(root):
+    if root.left != None:
+        inorder_traversal(root.left)
+    print(root.value)
+    if root.right != None:
+        inorder_traversal(root.right)
+
+def preorder_traversal(root):
+    print(root.value)
+    if root.left != None:
+        preorder_traversal(root.left)
+    if root.right != None:
+        preorder_traversal(root.right)
+
+def postorder_traversal(root):
+    if root.left != None:
+        postorder_traversal(root.left)
+    if root.right != None:
+        postorder_traversal(root.right)
+    print(root.value)
+
+root = None
+
+
+while True:
+    print("Press 1 to make the nodes")
+    print("Press 1.5 for the smallest value")
+    print("Press 2 to search for values")
+    print("Press 3 for inorder traversal")
+    print("Press 4 for preorder traversal")
+    print("Press 5 for postorder traversal")
+    print("Press 6 to exit")
+
+    choice = (input("Enter option: "))
+
+    if choice == "1":
+        n = int(input("What number of nodes do you want: "))
+        for i in range(n):
+            k = int(input("Insert the number for each node: "))
+            root = insert(root, k)
+
+    if choice == "1.5":
+        print(mostleft(root))
+
+    if choice == "2":
+        find = search(root, int(input("What number are you searching for: ")))
+        if find == -1:
+            print("Not Found")
+        else:
+            print("Key is found", find.value)
+        
+    elif choice == "3":
+        inorder_traversal(root)
+    elif choice == "4":
+        preorder_traversal(root)
+    elif choice == "5":
+        postorder_traversal(root)
+
+    elif choice >= "6":
+        break
